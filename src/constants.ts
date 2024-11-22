f5019e14ddee3abfea132a28998aee84ce039cb5
Henrique Gomes <henriquegomesjunqueira@gmail.com>
2024-11-21 21:53:49 -0300

    chore: change api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const URL = "http://127.0.0.1:3000";
+export const URL = "https://rails-chat-backend-413396343142.southamerica-east1.run.app";
 
 export function FormatDate(date: Date, withTime = true): string {
   if (!withTime) {
